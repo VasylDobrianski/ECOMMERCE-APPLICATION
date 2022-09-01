e2254d792e75db57cbc25c573d79844e9693e0aa
Vasyl <vasyl.b.dobrianski@gmail.com>
2022-09-01 17:42:30 -0500

    Address for Customer created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+        createCustomer();
+        createProduct();
+        createCategory();
+        createBalance();
+        createDiscount();
+
+
+
+    }
 }
