4dbe0727c2641dd8d63bf8d8da1616e897d7c62d
Vasyl <vasyl.b.dobrianski@gmail.com>
2022-09-01 19:45:55 -0500

    Address for Customer created

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,2 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
+
+    private String userName;
+    private String email;
+    private List<Address> address;
+
+    //overloaded constructors due to the need for two options
+
+    public Customer(String userName, String email) {
+        this.userName = userName;
+        this.email = email;
+    }
+
+    public Customer(String userName, String email, List<Address> address) {
+        this.userName = userName;
+        this.email = email;
+        this.address = address;
+    }
+
+
+
+    //type name;
+    //class=Address name=Address
+
+    //ArrayList<Address> abc = new ArrayList<>();
+    //List preferred - polymorphism
+    //Always put interface portion - Interface abc = new Class(); ---> polymorphism
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<Address> getAddress() {
+        return address;
+    }
+
 }
