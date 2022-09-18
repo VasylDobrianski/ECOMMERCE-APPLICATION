e6966918f1acaa26ab00fd617840afe97921abad
Vasyl <vasyl.b.dobrianski@gmail.com>
2022-09-18 00:22:20 -0500

    Scanner(MENU) - Started

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,25 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         DataGenerator.createCustomer();
-        DataGenerator.createProduct();
         DataGenerator.createCategory();
-//        DataGenerator.createBalance();
-//        DataGenerator.createDiscount();
+        DataGenerator.createProduct();
+        DataGenerator.createBalance();
+        DataGenerator.createDiscount();
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Select Customer:");
+        for (int i = 0; i < StaticConstants.CUSTOMER_LIST.size(); i++){
+            System.out.println("Type " + i + " for customer: " + StaticConstants.CUSTOMER_LIST.get(i).getUserName());
+        }
+
+        Customer customer = StaticConstants.CUSTOMER_LIST.get(scanner.nextInt());
+
+        //System.out.println("Customer selected: " + customer.getUserName()); (TEST LINE)
 
     }
 }
