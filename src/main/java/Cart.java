caa78df2d193bbe650830221d732dd789931bd3d
Vasyl <vasyl.b.dobrianski@gmail.com>
2022-09-18 07:06:48 -0500

    Cart - started

---
+++ src/main/java/Cart.java
@@ -0,0 +1,11 @@
+import java.util.Map;
+import java.util.UUID;
+
+public class Cart {
+
+    private Customer customer;
+    private UUID discountId;
+    private Map<Product,Integer> productMap;
+
+
+}
