ad73ab904096d54cb780ac511a8afe9912d45672
Vasyl <vasyl.b.dobrianski@gmail.com>
2022-09-18 00:39:27 -0500

    Scanner(MENU) - Extended input selections

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -18,8 +17,58 @@ public class Main {
         }
 
         Customer customer = StaticConstants.CUSTOMER_LIST.get(scanner.nextInt());
-
         //System.out.println("Customer selected: " + customer.getUserName()); (TEST LINE)
 
+        while (true){
+
+            System.out.println("What would you like to do --> Just type id for selection");
+
+            for (int i = 0; i < prepareMenuOptions().length; i++){
+                System.out.println(i + "-" + prepareMenuOptions()[i]);
+            }
+
+            int menuSelection = scanner.nextInt();
+
+            switch (menuSelection){
+                case 0: //List Categories
+
+                    break;
+                case 1: //List Products
+
+                    break;
+                case 2: //List Discount
+
+                    break;
+                case 3: //See Balance
+
+                    break;
+                case 4: //Add Balance
+
+                    break;
+                case 5: //Place Order
+
+                    break;
+                case 6: //See Cart
+
+                    break;
+                case 7: //Order Details
+
+                    break;
+                case 8: //See Address
+
+                    break;
+                case 9: //Close Application
+
+                    break;
+            }
+
+
+        }
+
     }
+
+    private static String[] prepareMenuOptions(){
+        return new String[]{"List Categories:","List Products:","List Discount","See Balance","Add Balance","Place Order","See Cart","Order Details","See Address","Close Application"};
+    }
+
 }
